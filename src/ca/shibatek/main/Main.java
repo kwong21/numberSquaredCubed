e4587122d78956a6ba0162c4232cb3b2bcd2694c
kwong21 <kwong338@my.bcit.ca>
2018-01-14 00:13:53 -0800

    Added try/catch block

--- src/ca/shibatek/main/Main.java
+++ src/ca/shibatek/main/Main.java
@@ -7,13 +7,28 @@ public class Main {
 	private static String border = "------------------------------";
 	
 	public static void main(String[] args) {
+		int upperLimit = 0;
 		int number = 0, square = 0, cube = 0;
 		int sumNumber = 0 , sumSquare = 0, sumCube = 0;
 		
 		Scanner reader = new Scanner(System.in);
-		System.out.print("Upper limit: ");
-		int upperLimit = reader.nextInt();
 		
+		while(true)
+		{
+			System.out.print("Upper limit: ");
+			String input = reader.next();
+			try 
+			{
+				upperLimit = Integer.parseInt(input);
+				break;
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("Please provide only numbers!");
+				
+			}
+		}
+	
 		System.out.printf("%8s %8s %8s\n", "number", "squared", "cubed");
 		System.out.println(border);
 		
